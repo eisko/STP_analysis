f7289c0f6f389f8b5d2687975f9a07d5eeda4e9d
eisko <buttaafly@gmail.com>
2023-08-25 10:44:39 -0400

    "test"

---
+++ brainreg_to_allen.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Align all brains to allen brain
+
+# make sure to `conda activate brainglobe` before running
+
+straight_brain="allen_10um"
+
+atlas="allen_mouse_10um"
+
+# Replace 'your_csv_file.csv' with the actual filename of your CSV file
+csv_file="/mnt/labNAS/Emily/STP_for_MAPseq/processed_data/file_path.csv"
+orientation='ars'
+out_folder="/mnt/labNAS/Emily/STP_for_MAPseq/3_brainreg_output/${straight_brain}_aligned"
+
+# Check if the CSV file exists
+if [ ! -f "$csv_file" ]; then
+  echo "CSV file not found: $csv_file"
+  exit 1
+fi
+
+# Check if out_folder already exists
+if [ -d "$out_folder" ]; then
+  echo "Folder '$out_folder' already exists."
+else
+  # Create the folder if it doesn't exist
+  mkdir "$out_folder"
+  echo "Folder '$out_folder' created successfully."
+fi
+
+
+# Read the CSV file line by line and extract the first and second columns
+while IFS= read -r line; do
+  # Extract the first column (filename identifier)
+  name=$(echo "$line" | cut -d ',' -f 1)
+
+  # Extract the second column (file path)
+  file_path=$(echo "$line" | cut -d ',' -f 2)
+  echo $file_path
+
+  # create subfolders for each aligned brain
+  # Check if out_folder already exists
+  if [ -d "${out_folder}/${name}_brainreg_${straight_brain}" ]; then
+    echo "Folder '${name}_brainreg_${straight_brain}' already exists."
+  else
+    # Create the folder if it doesn't exist
+    mkdir "${out_folder}/${name}_brainreg_${straight_brain}"
+    echo "Folder '${name}_brainreg_${straight_brain}' created successfully."
+  fi
+
+  # run brainreg
+  brainreg $file_path "${out_folder}/${name}_brainreg_${straight_brain}" \
+  -v 50 20 20 --orientation $orientation --atlas $atlas
+
+done < $csv_file
