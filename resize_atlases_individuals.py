5432aa9ad829b3885a37830575a84200bcc0aef6
eisko <buttaafly@gmail.com>
2023-09-06 18:30:19 -0400

    improve individual alignments

---
+++ resize_atlases_individuals.py
@@ -0,0 +1,48 @@
+# script to resize registered atlas and hemispheres
+# processes aligned allen_10um atlas, output from brainreg_to_allen.sh
+
+import pandas as pd
+import numpy as np
+from skimage.transform import resize
+import tifffile as tf
+
+home_dir = "/mnt/labNAS/"
+# home_dir = "/Volumes/Data/"
+
+meta_path=home_dir+"Emily/STP_for_MAPseq/3_brainreg_output/aligned_file_meta.csv"
+metadata = pd.read_csv(meta_path, sep=" ")
+# metadata = pd.read_csv("STP_metadata.csv")
+
+# in files, out from brainreg_to_allen
+straight_brain="allen_10um"
+in_path=home_dir+"Emily/STP_for_MAPseq/3_brainreg_output/"+straight_brain+"_aligned/"
+out_path = home_dir+"Emily/STP_for_MAPseq/4_python_output/input_tifs/resized_atlases/"
+
+for i in range(metadata.shape[0]):
+    print("working on", metadata.loc[i,"brain"])
+    in_folder_path = in_path+metadata.loc[i,"brain"]+"_brainreg_"+straight_brain+"/"
+    in_file_atlas = in_folder_path+"registered_atlas.tiff"
+    in_file_hemis = in_folder_path+"registered_hemispheres.tiff"
+
+    # resize atlas/hemi file
+    atlas = tf.imread(in_file_atlas)
+    hemis = tf.imread(in_file_hemis)
+    size = eval(metadata.loc[i,"p05_size"]) # read_csv reads in tuple as string, eval transforms it into tuple
+    atlas_resize = resize(atlas, size,
+                 mode='edge',
+                 anti_aliasing=False,
+                 anti_aliasing_sigma=None,
+                 preserve_range=True,
+                 order=0)
+    hemis_resize = resize(hemis, size,
+                 mode='edge',
+                 anti_aliasing=False,
+                 anti_aliasing_sigma=None,
+                 preserve_range=True,
+                 order=0)
+    
+    # save resized images
+    tf.imwrite(out_path+"allen_10um_to_"+metadata.loc[i, 'brain']+"_atlas_RESIZED.tiff", 
+               atlas_resize)
+    tf.imwrite(out_path+"allen_10um_to_"+metadata.loc[i, 'brain']+"_hemis_RESIZED.tiff", 
+               hemis_resize)
